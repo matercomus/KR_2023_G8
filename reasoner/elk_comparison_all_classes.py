ecd30cff959df830cedd6264ba53789a7a32fcd0
matercomus <matek.kedzia@gmail.com>
2023-11-22 02:10:01 +0100

    modified all_classes to show progress

--- reasoner/elk_comparison_all_classes.py
+++ reasoner/elk_comparison_all_classes.py
@@ -1,10 +1,13 @@
 import argparse
+import glob
 import json
+import os
 import time
 
 import numpy as np
 from no_java_lib import *
 from py4j.java_gateway import JavaGateway
+from tqdm import tqdm
 
 
 def calculate_stats(execution_times):
@@ -48,60 +51,84 @@ def process_class(class_name, reasoner_EL, reasoner_ELK, elFactory, formatter, a
 
     return result, end_time_EL - start_time_EL, end_time_ELK - start_time_ELK
 
+
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser(description="EL Reasoner")
-    arg_parser.add_argument("ontology_file", type=str, help="Path to the ontology file")
+    arg_parser.add_argument("ontology_file", type=str, help="Path to the ontology file or directory containing XML files")
     arg_parser.add_argument("-l", "--limit", type=int, help="Limit on the number of classes to process")
     arg_parser.add_argument("-v", "--verbose", action="store_true", help="Print details while processing")
+    arg_parser.add_argument("-p", "--progress", action="store_true", help="Show progress bar")
     args = arg_parser.parse_args()
 
     gateway = JavaGateway()
     parser = gateway.getOWLParser()
     formatter = gateway.getSimpleDLFormatter()
-    ontology_ELK = parser.parseFile(args.ontology_file)
-    ontology_EL = load_ontology(args.ontology_file)
 
-    reasoner_EL = ELReasoner(ontology_EL)
-    all_classes = reasoner_EL.compute_all_classes()
-    if args.limit is not None:
-        all_classes = all_classes[:args.limit]
+    if os.path.isdir(args.ontology_file):
+        ontology_files = glob.glob(os.path.join(args.ontology_file, "*.xml"))
+    else:
+        ontology_files = [args.ontology_file]
 
-    gateway.convertToBinaryConjunctions(ontology_ELK)
-    elFactory = gateway.getELFactory()
-    reasoner_ELK = gateway.getELKReasoner()
-    reasoner_ELK.setOntology(ontology_ELK)
+    aggregate_results = {}
 
-    results = []
-    execution_times_EL = []
-    execution_times_ELK = []
+    for ontology_file in ontology_files:
+        if args.verbose or args.progress:
+            print(f"Processing {ontology_file}")
 
-    for class_name in all_classes:
-        result, time_elapsed_EL, time_elapsed_ELK = process_class(class_name, reasoner_EL, reasoner_ELK, elFactory, formatter, args)
-        results.append(result)
-        execution_times_EL.append(time_elapsed_EL)
-        execution_times_ELK.append(time_elapsed_ELK)
+        ontology_ELK = parser.parseFile(ontology_file)
+        ontology_EL = load_ontology(ontology_file)
 
-    total_time_EL, avg_time_EL, std_dev_EL = calculate_stats(execution_times_EL)
-    total_time_ELK, avg_time_ELK, std_dev_ELK = calculate_stats(execution_times_ELK)
+        reasoner_EL = ELReasoner(ontology_EL)
+        all_classes = reasoner_EL.compute_all_classes()
+        if args.limit is not None:
+            all_classes = all_classes[:args.limit]
 
-    if args.verbose:
-        print(f"ELReasoner total time: {total_time_EL}, average time: {avg_time_EL}, standard deviation: {std_dev_EL}")
-        print(f"ELK total time: {total_time_ELK}, average time: {avg_time_ELK}, standard deviation: {std_dev_ELK}")
+        gateway.convertToBinaryConjunctions(ontology_ELK)
+        elFactory = gateway.getELFactory()
+        reasoner_ELK = gateway.getELKReasoner()
+        reasoner_ELK.setOntology(ontology_ELK)
 
-    # Add the stats to the results
-    stats = {
-        "ELReasoner": {
-            "total_time": total_time_EL,
-            "average_time": avg_time_EL,
-            "std_dev": std_dev_EL
-        },
-        "ELK": {
-            "total_time": total_time_ELK,
-            "average_time": avg_time_ELK,
-            "std_dev": std_dev_ELK
+        results = []
+        execution_times_EL = []
+        execution_times_ELK = []
+
+        if args.progress:
+            all_classes = tqdm(all_classes)
+
+        for class_name in all_classes:
+            result, time_elapsed_EL, time_elapsed_ELK = process_class(class_name, reasoner_EL, reasoner_ELK, elFactory, formatter, args)
+            results.append(result)
+            execution_times_EL.append(time_elapsed_EL)
+            execution_times_ELK.append(time_elapsed_ELK)
+
+        total_time_EL, avg_time_EL, std_dev_EL = calculate_stats(execution_times_EL)
+        total_time_ELK, avg_time_ELK, std_dev_ELK = calculate_stats(execution_times_ELK)
+
+        if args.verbose:
+            print(f"ELReasoner total time: {total_time_EL}, average time: {avg_time_EL}, standard deviation: {std_dev_EL}")
+            print(f"ELK total time: {total_time_ELK}, average time: {avg_time_ELK}, standard deviation: {std_dev_ELK}")
+            print()
+
+        # Add the stats to the results
+        stats = {
+            "ELReasoner": {
+                "total_time": total_time_EL,
+                "average_time": avg_time_EL,
+                "std_dev": std_dev_EL
+            },
+            "ELK": {
+                "total_time": total_time_ELK,
+                "average_time": avg_time_ELK,
+                "std_dev": std_dev_ELK
+            }
         }
-    }
-    results.append({"statistics": stats})
+        results.append({"statistics": stats})
 
-    with open("results.json", "w") as f:
-        json.dump(results, f, indent=2)
+        with open(f"{os.path.basename(ontology_file)}.results.json", "w") as f:
+            json.dump(results, f, indent=2)
+
+        aggregate_results[os.path.basename(ontology_file)] = stats
+
+        # Save the aggregate results after each ontology
+        with open("aggregate_results.json", "w") as f:
+            json.dump(aggregate_results, f, indent=2)
